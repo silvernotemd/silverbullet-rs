539fad322fff499a45418794307bfcf7d8c2bebc
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2025-12-20 21:46:11 +0100

    feat: expand shell handling
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- core/src/shell.rs
+++ core/src/shell.rs
@@ -1,8 +1,38 @@
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+#[error("Failed to run command")]
+pub struct Error {}
+
+pub trait Handler {
+    fn handle(&self, request: Request) -> Result<Response, Error>;
+}
 
 #[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Request {
+    pub cmd: String,
+    pub args: Vec<String>,
+    pub stdin: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub struct Response {
     pub code: u16,
     pub stdout: String,
     pub stderr: String,
+}
+
+pub struct NoShell {}
+
+impl Handler for NoShell {
+    fn handle(&self, _request: Request) -> Result<Response, Error> {
+        Ok(Response {
+            code: 1,
+            stdout: "".to_string(),
+            stderr: "Not supported".to_string(),
+        })
+    }
 }
